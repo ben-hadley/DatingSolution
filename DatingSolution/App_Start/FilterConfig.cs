d2bd6a38fcc74d6b0228634b052f8746acc55e44
ben-hadley <b1@austral.se>
2015-12-17 20:58:29 +0100

    La till projekt
    La till projekt

---
+++ DatingSolution/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DatingSolution
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ DatingSolution/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DatingSolution.MvcApplication" Language="C#" %>
